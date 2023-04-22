fcf820fe3a56e965b05c79e57477416e8890e3b9
zchhhhh <2069820192@qq.com>
2023-04-22 21:32:19 +0800

    feat: 新增demo-10-mail

---
+++ demo-10-mail/src/main/java/cdu/zch/demo10mail/Demo10MailApplication.java
@@ -0,0 +1,13 @@
+package cdu.zch.demo10mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo10MailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo10MailApplication.class, args);
+    }
+
+}
