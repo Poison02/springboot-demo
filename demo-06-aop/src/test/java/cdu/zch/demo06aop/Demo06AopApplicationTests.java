e3e731ba0e32701ea7b2ae682996de7ad4cb61f3
zchhhhh <2069820192@qq.com>
2023-04-22 21:24:47 +0800

    feat: 新增demo-06-aop

---
+++ demo-06-aop/src/test/java/cdu/zch/demo06aop/Demo06AopApplicationTests.java
@@ -0,0 +1,13 @@
+package cdu.zch.demo06aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo06AopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
