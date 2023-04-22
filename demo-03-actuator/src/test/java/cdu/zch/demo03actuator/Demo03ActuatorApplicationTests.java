d3ef738103b0a843a3819936b459b0b289b2dac0
zchhhhh <2069820192@qq.com>
2023-04-22 21:11:59 +0800

    feat: 新增demo-03-actuator

---
+++ demo-03-actuator/src/test/java/cdu/zch/demo03actuator/Demo03ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package cdu.zch.demo03actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo03ActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
