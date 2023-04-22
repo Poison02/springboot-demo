3f10372724891d3f9535d954422cba2dbfcccd4d
zchhhhh <2069820192@qq.com>
2023-04-22 21:24:21 +0800

    feat: 新增demo-05-logback

---
+++ demo-05-logback/src/test/java/cdu/zch/demo05logback/Demo05LogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package cdu.zch.demo05logback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo05LogbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
